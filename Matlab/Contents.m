a8d40fa94bcb1e19d5366c3780cdb0aa5ecb8602
sokratis <smakrogiannis@desu.edu>
2015-06-01 12:29:00 -0400

    Initial commit: documentation for contents of TIDAQ matlab files.

---
+++ Matlab/Contents.m
@@ -0,0 +1,7 @@
+% MATLAB
+%
+% Files
+%   AnalyzePQCTImageInFeatureSpace      - syntax: AnalyzePQCTImageInFeatureSpace(filename);
+%   BatchAnalyzePQCTImageInFeatureSpace - Read image list.
+%   ConvertPQCTImageToNifti             - syntax: niftiImage = ConvertPQCTImageToNifti( pqctFilename, voxelSize );
+%   ReadPQCTImage                       - Read a PQCT image type.
